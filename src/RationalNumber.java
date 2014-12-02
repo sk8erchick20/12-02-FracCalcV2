bfda2aa1eba32ba059e76ae460503881678a2cd2
Karishma <tikkysood@yahoo.com>
2014-12-02 08:15:32 -0700

    Update RationalNumber.java

--- src/RationalNumber.java
+++ src/RationalNumber.java
@@ -1,1 +1,39 @@
 // Write a class called RationalNumber that represents a fraction with an integer numerator and denominator
+public class RationalNumber
+{
+  int x;
+  int y;
+  public rationalNumber()
+  {
+    rationalNumber a;
+  }
+//  public rationalNumber(int numerator, int denominator)
+//  {
+//    if(denominator<=0)
+//    {
+  public int getDenominator()
+  {
+    return denom;
+  }
+  public int getNumerator()
+  {
+    return numer;
+  }
+  public String toString()
+  {
+    public int GreatestCommonDivisor(int first, int second) 
+    {
+      int temp;
+      while (second != 0) 
+      {
+        temp = second;
+        second = first % second;
+        first = temp;
+      }        
+      return first;
+    }
+    return numerator"/"denom;
+  }
+  public void add(RationalNumber frac)
+  {
+    
