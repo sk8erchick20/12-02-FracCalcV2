16658427fc198c7b30bd4ce2ee6324ed922677c0
Karishma <tikkysood@yahoo.com>
2014-12-02 22:31:29 -0700

    Update RationalNumber.java

--- src/RationalNumber.java
+++ src/RationalNumber.java
@@ -1,16 +1,17 @@
-// Write a class called RationalNumber that represents a fraction with an integer numerator and denominator
 public class RationalNumber
 {
-  int x;
-  int y;
-  public rationalNumber()
+  int numer;
+  int denom;
+  public RationalNumber()
   {
-    rationalNumber a;
+    numer=0;
+    denom=0;
   }
-//  public rationalNumber(int numerator, int denominator)
-//  {
-//    if(denominator<=0)
-//    {
+  public RationalNumber(int numerator, int denominator)
+  {
+    numer=numerator;
+    denom=denominator;
+  }
   public int getDenominator()
   {
     return denom;
@@ -19,21 +20,47 @@ public class RationalNumber
   {
     return numer;
   }
+  public int GreatestCommonDivisor(int first, int second)
+  {
+    int temp;
+    while (second != 0)
+    {
+      temp = second;
+      second = first % second;
+      first = temp;
+    }
+    return first;
+  }
   public String toString()
   {
-    public int GreatestCommonDivisor(int first, int second) 
-    {
-      int temp;
-      while (second != 0) 
-      {
-        temp = second;
-        second = first % second;
-        first = temp;
-      }        
-      return first;
-    }
-    return numerator"/"denom;
+    return (numer+"/"+denom);
   }
   public void add(RationalNumber frac)
   {
-    
+    int x=frac.getDenominator();
+    int y=frac.getNumerator();
+    numer=(numer*x)+(y*denom);
+    denom=denom*x;
+  }
+  public void subtract(RationalNumber frac)
+  {
+    int y=frac.getNumerator();
+    int x=frac.getDenominator();
+    numer=(numer*x)-(y*denom);
+    denom=denom*x;
+  }
+  public void multiply(RationalNumber frac)
+  {
+    int y=frac.getNumerator();
+    int x=frac.getDenominator();
+    denom=denom*x;
+    numer=numer*y;
+  }
+  public void divide(RationalNumber frac)
+  {
+    int y=frac.getNumerator();
+    int x=frac.getDenominator();
+    numer=numer*x;
+    denom=denom*y;
+  }
+}
